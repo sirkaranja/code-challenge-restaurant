9d07762c37ca3f3f86cc52f85eee5bca62668307
+ <+>
2023-06-05 12:15:18 +0300

    restaurant orm:

--- lib/Customer.py
+++ lib/Customer.py
@@ -1,39 +1,97 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
-engine= create_engine('sqlite:///customer.db')
-session = sessionmaker(bind=engine)
 
 
 class Customer(Base):
-    __tablename__= 'customers'
+    __tablename__ = 'customers'
+
     id = Column(Integer, primary_key=True)
-    surname = Column(String)
+    given_name = Column(String)
     family_name = Column(String)
-
-    all_customers=[]
-
-    def __init__(self, surname, family_name):
-        self.last_name= surname
-        self.family_name=family_name
-        Customer.all_customers.append(self)
-
-    def surname(self):
-        return self.last_name
-
-    def family_name(self):
-        return self.family_name
+    reviews = relationship('Review', back_populates='customer')
 
     def full_name(self):
-        return f"{self.family_name} {self.last_name}"
+        return f"{self.given_name} {self.family_name}"
+
+    def add_review(self, restaurant, rating):
+        review = Review(customer=self, restaurant=restaurant, rating=rating)
+        self.reviews.append(review)
+
+    def restaurants(self):
+        return [review.restaurant for review in self.reviews]
+
+    def num_reviews(self):
+        return len(self.reviews)
 
     @classmethod
-    def all(cls):
-        return cls.all_customers
-        
+    def find_by_name(cls, name):
+        session = Session()
+        customer = session.query(cls).filter(cls.full_name() == name).first()
+        session.close()
+        return customer
 
+    @classmethod
+    def find_all_by_given_name(cls, given_name):
+        session = Session()
+        customers = session.query(cls).filter(cls.given_name == given_name).all()
+        session.close()
+        return customers
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    reviews = relationship('Review', back_populates='restaurant')
+
+
+    def __init__(self, name):
+        self.name = name
+
+
+    def average_star_rating(self):
+        total_ratings = sum(review.rating for review in self.reviews)
+        num_reviews = len(self.reviews)
+        if num_reviews == 0:
+            return 0
+        return total_ratings / num_reviews
+
+    def customers(self):
+        return [review.customer for review in self.reviews]
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    rating = Column(Integer)
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
+
+
+    def __init__(self, restaurant,restaurant_customer, customer_rating):
+        self.restaurant = restaurant
+        self.restaurant_customer = restaurant_customer
+        self.customer_rating = customer_rating
+        Review.all_reviews.append(self)
+
+    def rating(self):
+        return self.customer_rating
+    
+    @classmethod
+    def all(cls):
+        for review in cls.all_reviews:
+            return review
+
+
+engine = create_engine('sqlite:///restaurant.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 
 
